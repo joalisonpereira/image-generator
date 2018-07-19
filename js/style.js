5c650006898873c934c7d9e2ab51b6d2219564c5
joalisonpereira <joalison.pereira@hotmail.com>
2018-07-19 16:43:12 -0300

    Modificações estilo #es6

--- js/style.js
+++ js/style.js
@@ -1,8 +1,51 @@
-const fadeInElements=document.querySelectorAll(".fadeIn");
-window.addEventListener("load", ()=>{
-	for(let element of fadeInElements){
-		setTimeout(()=>{
-			element.classList.add('activeFadeIn');
-		},element.getAttribute("data-load-time"));
-	}
+const fadeInElements = document.querySelectorAll(".fadeIn");
+const images = document.querySelectorAll(".images img");
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+let checks = 0;
+window.addEventListener("load", () => {
+    for (let element of fadeInElements) {
+        setTimeout(() => {
+            element.classList.add('activeFadeIn');
+        }, element.getAttribute("data-load-time"));
+    }
+    for (let element of checkboxes) {
+        element.addEventListener("change", () => {
+            if (isChecked(element)) {
+                if (++checks == 1) {
+                    addEffectImage(element.id);
+                } else {
+                    clearEffects();
+                    addEffectImage("grayscale-blur");
+                }
+            } else {
+                clearEffects();
+                if (--checks == 1) {
+                    let stayClass = (element.id === "grayscale") ? "blur" : "grayscale";
+                    addEffectImage(stayClass);
+                }
+            }
+        });
+    }
 });
+
+function addEffectImage(effect) {
+    for (let image of images) {
+        image.classList.add(effect);
+    }
+}
+
+function removeEffectImage(effect) {
+    for (let image of images) {
+        image.classList.remove(effect);
+    }
+}
+
+function isChecked(element) {
+    return element.checked === true;
+}
+
+function clearEffects() {
+    removeEffectImage("grayscale");
+    removeEffectImage("blur");
+    removeEffectImage("grayscale-blur");
+}
